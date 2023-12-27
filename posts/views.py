6d8aa29128709ecf37beaf81ab26d24389dee047
Marchuk Maxim <maxxik007@gmail.com>
2023-12-27 20:56:34 +0100

    [MOD]: Mocked up list of posts and saved to database. Created page to display posts.

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from posts.models import Post
 
 def index_view(request):
-    return render(request, "index.html")
+    posts = Post.objects.all()
+    return render(request, "index.html", {'posts': posts})
